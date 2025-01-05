61364dafa8be54a73ba33c24d724f9fea121327b
Nazmul Islam <41779390+nazmulham@users.noreply.github.com>
2025-01-06 03:08:25 +0600

    Add files via upload

---
+++ cloud/include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>mZWfoZ8=');
+
+$data['mostawifi'] = array ('1'=>'mostawifi!33b6044646ad.sn.mynetname.net','mostawifi@|@admin','mostawifi#|#hpKspaaepZVkaGNieFRW','mostawifi%Mosta Wifi','mostawifi^mosta.net','mostawifi&Tk','mostawifi*10','mostawifi(1','mostawifi)','mostawifi=10','mostawifi@!@enable');
