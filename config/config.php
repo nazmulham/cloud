95cd7e2ae9782658bc791a12aa4e0fc4c94c7735
Nazmul Islam <41779390+nazmulham@users.noreply.github.com>
2025-01-06 02:53:05 +0600

    Add files via upload
    just upload

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>mZWfoZ8=');
+$data['mostawifi'] = array ('1'=>'mostawifi!172.16.16.1','mostawifi@|@Nazmul','mostawifi#|#ppKspaae','mostawifi%Mosta Wifi','mostawifi^mosta.net','mostawifi&Tk','mostawifi*01717237334','mostawifi(','mostawifi)','mostawifi=disable','mostawifi@!@enable','mostawifi#!#');
